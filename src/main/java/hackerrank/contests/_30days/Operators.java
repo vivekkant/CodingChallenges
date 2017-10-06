837027a2564724d0ea8592e1f947268ea25dde7b
Kant <212542@cognizant.com>
2017-10-06 15:11:52 +0530

    30 days of code

---
+++ src/main/java/hackerrank/contests/_30days/Operators.java
@@ -0,0 +1,24 @@
+package hackerrank.contests._30days;
+
+import java.util.Scanner;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+      
+        // Write your calculation code here.
+        double tip = tipPercent * mealCost / 100;
+        double tax = taxPercent * mealCost / 100;
+      
+        // cast the result of the rounding operation to an int and save it as totalCost 
+        int totalCost = (int) Math.round(mealCost + tip + tax);
+      
+        // Print your result
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
+    }
+}
