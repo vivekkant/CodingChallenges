9886c9515c3932f1d202f4be58f5824328600e29
Kant <212542@cognizant.com>
2017-10-24 06:03:01 +0530

    30 days of code - binary numbers

---
+++ src/main/java/hackerrank/contests/_30days/BinaryNumbers.java
@@ -0,0 +1,29 @@
+package hackerrank.contests._30days;
+
+import java.util.Scanner;
+
+public class BinaryNumbers {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        
+        int max = 0;
+        int current = 0;
+        while (n > 0) {
+        	int b = n % 2;
+        	if (b == 0) {
+        		max = (current > max) ? current : max;
+        		current = 0;
+        	}
+        	else {
+        		current++;
+        	}
+        	n = n / 2;
+        	System.out.print(b);
+        }
+        System.out.println();
+        System.out.println(max);
+	}
+
+}
