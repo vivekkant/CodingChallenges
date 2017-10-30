f66d9fc866a223fc6e1aa7154d6af4605d6d4cd9
Kant <212542@cognizant.com>
2017-10-30 21:40:02 +0530

    30 days of code

---
+++ src/main/java/hackerrank/contests/_30days/BST.java
@@ -0,0 +1,51 @@
+package hackerrank.contests._30days;
+
+import java.util.Scanner;
+
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+
+
+public class BST {
+	
+	public static int getHeight(Node root){
+	    int lheight = (root.left == null) ? -1 : getHeight(root.left);
+	    int rheight = (root.right == null) ? -1 : getHeight(root.right);
+	    return (lheight > rheight) ? lheight + 1 : rheight + 1;
+	}
+	
+	public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            int height=getHeight(root);
+            System.out.println(height);
+        }	
+}
